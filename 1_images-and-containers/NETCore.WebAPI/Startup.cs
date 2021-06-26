7401d471695897cb6f862f2697ecff5ab8f4d425
Bruno Leone <leonecbruno@gmail.com>
2021-06-26 00:19:35 -0300

    Ultimo commit

--- 1_images-and-containers/NETCore.WebAPI/Startup.cs
+++ 1_images-and-containers/NETCore.WebAPI/Startup.cs
@@ -36,8 +36,6 @@ namespace NETCore.WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
